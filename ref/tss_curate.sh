0f8c8b4c11c8f2ed04487e12d2c6ec6d9be94418
markziemann <mark.ziemann@gmail.com>
2023-11-29 13:59:09 +1100

    stupid chromosome names

--- ref/tss_curate.sh
+++ ref/tss_curate.sh
@@ -5,6 +5,10 @@
 # get the promoter coordinates from the GTF file with gtftools
 gtftools.py -t tss_regions.bed -w 1000-300 gencode.v44.primary_assembly.annotation.gtf
 
+sed -i 's/^/chr/' tss_regions.bed
+
+bedtools sort -i tss_regions.bed > tss_regions_sorted.bed && mv tss_regions_sorted.bed tss_regions.bed
+
 # now collapse the overlapping features
 for GID in $(cut -f6 tss_regions.bed | sort -u ) ; do
   GENESYMBOL=$(grep -w $GID tss_regions.bed |head -1 | cut -f7)
